b771c833eb96b28e96e278bb994f99afdabb893b
Muhammad Bilal <mohammadbillal268@gmail.com>
2024-01-17 11:08:02 +0500

    queston 21 tk ho gae functions k

--- app.js
+++ app.js
@@ -160,3 +160,130 @@ function handleConcate(a, b, c) {
   console.log(`Ans 05 : ${a},${b},${c}`);
 }
 handleConcate(bool, "Bilal", 28);
+/************** Q No 06(Code a function that has 2 parameters. Concatenate them and 
+assign the result to a variable that hasn't been declared 
+beforehand. )**************/
+function handleTwoPara(a, b) {
+  var ab = a + b;
+  return ab;
+}
+var ab = handleTwoPara("Muhammad ", "Bilal");
+console.log(`Ans 06 : ${ab}`);
+/************** Q No 07(Code a function that has three parameters. Multiply them and 
+ assign them to a variable that hasn't been declared yet. )**************/
+function handleThreePara(a, b, c) {
+  var abc = a * b * c;
+  return abc;
+}
+var abc = handleThreePara(2, 3, 4);
+console.log(`Ans 07 : ${abc}`);
+/************** Q No 08(Write a JavaScript function that takes an array of numbers as 
+input and returns the average of those numbers. )**************/
+function handleAvg(numArr) {
+  // console.log(numArr);
+  var avg = 0;
+  for (i = 0; i < numArr.length; i++) {
+    avg = avg + numArr[i];
+  }
+  // console.log(avg);
+  avg = avg / numArr.length;
+  return avg.toFixed(3);
+}
+console.log(`Ans 08 : Avg is ${handleAvg([2, 4, 6, 8, 10, 20])}`);
+
+/************** Q No 09(Write a JavaScript function that takes two parameters and returns 
+their sum. )**************/
+
+function handleSum(num1, num2) {
+  return num1 + num2;
+}
+console.log(`Ans 09 : sum of two Parameters ${handleSum(5, 5)}`);
+
+/************** Q No 15(Write a function which tells the presense of (word) in an 
+array = ['zaid','haris','raza','abubakar','hassan','hussain','fatima'] 
+result should be in true or false )**************/
+
+function handleCheckVal(name) {
+  var bool = false;
+  array = ["zaid", "haris", "raza", "abubakar", "hassan", "hussain", "fatima"];
+  for (i = 0; i < array.length - 1; i++) {
+    // console.log(array[i]);
+    if (name === array[i]) {
+      bool = true;
+      console.log(name);
+      break;
+    }
+  }
+  return bool;
+}
+console.log(`Ans 15 : ${handleCheckVal("raza")}`);
+console.log(`Ans 15 : ${handleCheckVal("bilal")}`);
+console.log(`Ans 15 : ${handleCheckVal("hussain")}`);
+
+/************** Q No 17(write a function which reverse array = ['a','b','c','d','e'] 
+Hint: arr.reverse() )**************/
+array = ["a", "b", "c", "d", "e"];
+function handleRev(arr) {
+  return arr.reverse();
+}
+var revArr = handleRev(array);
+console.log(`Ans 17 : ${revArr}`);
+
+// /************** Q No 18(Write a JavaScript function that reverses a number. )**************/
+function handleRevNum(num) {
+  num = num.toString().split("").reverse().join("");
+  return num;
+}
+console.log(`Ans 18 : ${handleRevNum(32243)}`);
+
+// Q No 19(Write a JavaScript function that checks whether a passed string is a palindrome or not)
+function handlePalindrom() {
+  var name = prompt("Enter Word to Chek Palindrome");
+  name = name.split("");
+  console.log(name);
+  var name1 = [];
+  var a = 0;
+  for (i = name.length; i > 0; i--) {
+    name1[a] = name[i - 1];
+    a++;
+    // name1.push(name[i - 1]);
+  }
+  console.log(name1);
+  name = name.join("");
+  console.log(name);
+  name1 = name1.join("");
+  console.log(name1);
+  if (name === name1) {
+    console.log("Ans 19 : Word is Palindrom");
+  } else {
+    console.log("Ans 19 : Word is Not Palindrom");
+  }
+}
+handlePalindrom();
+
+// Q No 20(Write a JavaScript function that accepts a string as a
+// parameter and converts the first letter of each word into upper
+// case.)
+
+function handleCaps(word) {
+  word = word.split(" ");
+  console.log(word);
+  for (i = 0; i < word.length; i++) {
+    // word[i] = word[i][0].toUpperCase() + word[i].slice(1);
+    word[i] = word[i].slice(0, 1).toUpperCase() + word[i].slice(1);
+    // word[i] = word[i].charAt(0).toUpperCase() + word[i].split(1);
+  }
+  console.log(word);
+  console.log(`Ans 20 : ${word.join(" ")}`);
+}
+handleCaps("the quick brown fox");
+
+// Q No 21(Write a JavaScript function that takes an array of numbers
+// and finds the lowest and greatest numbers, respectively.)
+
+function handleMAxMin(arr) {
+  console.log(...arr);
+  console.log(`Ans 21 : Max Number ${Math.max(...arr)}`);
+  console.log(`Ans 21 : Min Number ${Math.min(...arr)}`);
+}
+handleMAxMin([33, 4, 5, 7, 8]);
